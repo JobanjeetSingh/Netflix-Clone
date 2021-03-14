df151f95a9fd87e11744079b3e7b6d8e18ea7eef
Jobanjeet  Singh <jobanjeetsingh@Jobanjeets-Mac-mini.local>
2021-03-14 16:37:44 -0700

    Final project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./app";
+import { GlobalStyles } from "./global-styles";
+import { FirebaseContext } from "./context/firebase";
+
+const config = {
+  apiKey: "AIzaSyAblHdoAj7M_ZR2DET72QDztBGCaVzgXHg",
+  authDomain: "joban-netflix.firebaseapp.com",
+  databaseURL: "https://joban-netflix-default-rtdb.firebaseio.com",
+  projectId: "joban-netflix",
+  storageBucket: "joban-netflix.appspot.com",
+  messagingSenderId: "78614670187",
+  appId: "1:78614670187:web:b2623b895c0b8e03003a2e",
+};
+
+const firebase = window.firebase.initializeApp(config);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
